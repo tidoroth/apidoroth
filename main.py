b365d9e6aa800de14d298a5acd14a573f2b697d0
Doroth <130710343+tidoroth@users.noreply.github.com>
2023-05-07 15:25:36 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from typing import Union
+from fastapi import FastAPI, HTTPException
+import pandas as pd
+import json
+import jwt
+
+df = pd.read_csv('agrogalaxyData.csv')
+df = df.fillna('')
+dfDict = df.to_dict(orient='records')
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return dfDict
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
